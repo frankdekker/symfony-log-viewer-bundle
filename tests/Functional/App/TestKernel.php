0a0d154c7dd0e28e551c1986e9331cf5e766f658
Frank Dekker <fdekker@123inkt.nl>
2023-12-24 17:38:11 +0100

    Add test env

---
+++ tests/Functional/App/TestKernel.php
@@ -0,0 +1,55 @@
+<?php
+declare(strict_types=1);
+
+namespace FD\SymfonyLogViewerBundle\Tests\Functional\App;
+
+use Exception;
+use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Symfony\Bundle\MonologBundle\MonologBundle;
+use Symfony\Bundle\TwigBundle\TwigBundle;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\HttpKernel\Bundle\BundleInterface;
+use Symfony\Component\HttpKernel\Kernel;
+
+final class TestKernel extends Kernel
+{
+    public function __construct(string $environment, bool $debug)
+    {
+        parent::__construct($environment, $debug);
+    }
+
+    /**
+     * @return iterable<int|string, BundleInterface>
+     */
+    public function registerBundles(): iterable
+    {
+        return [
+            new FrameworkBundle(),
+            new TwigBundle(),
+            new MonologBundle()
+        ];
+    }
+
+    public function getLogDir(): string
+    {
+        return dirname(__DIR__, 3) . '/tmp/';
+    }
+
+    public function getCacheDir(): string
+    {
+        return dirname(__DIR__, 3) . '/tmp/';
+    }
+
+    public function getProjectDir(): string
+    {
+        return __DIR__;
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function registerContainerConfiguration(LoaderInterface $loader): void
+    {
+        $loader->load($this->getProjectDir() . "/config/config.yml");
+    }
+}
