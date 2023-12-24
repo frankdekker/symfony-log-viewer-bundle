fa160b9ce89795413203112927d58908d4d266d4
Frank Dekker <fdekker@123inkt.nl>
2023-12-24 21:06:10 +0100

    Add docker

---
+++ dev/docker/php-fpm/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+composer install --no-interaction --optimize-autoloader --no-scripts
+
+mkdir -p /app/var/cache/prod
+chmod -R a+rw /app/var/cache
+chown www-data:www-data -R /app/var/cache
+
+php bin/console cache:clear
+php bin/console assets:install
+
+printenv > /etc/environment
+mkdir -p /app/var/log
+chmod -R a+rw /app/var
+chown www-data:www-data -R /app/var
+
+exec supervisord -c /etc/supervisor/conf.d/supervisord.conf
--- dev/src/Kernel.php
+++ dev/src/Kernel.php
@@ -4,10 +4,24 @@ declare(strict_types=1);
 
 namespace App;
 
+use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\HttpKernel\Bundle\BundleInterface;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    /**
+     * @return iterable<int|string, BundleInterface>
+     */
+    public function registerBundles(): iterable
+    {
+        return [
+            new FrameworkBundle(),
+            //new TwigBundle(),
+            //new MonologBundle()
+        ];
+    }
 }
