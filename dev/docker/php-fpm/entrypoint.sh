26da3ee02a68e998681cdce5210e0fb7b5bad220
Frank Dekker <fdekker@123inkt.nl>
2023-12-24 22:01:02 +0100

    Add dev env

--- dev/docker/php-fpm/entrypoint.sh
+++ dev/docker/php-fpm/entrypoint.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 set -e
 
-composer install --no-interaction --optimize-autoloader --no-scripts
+composer install --no-interaction --optimize-autoloader
 
-mkdir -p /app/var/cache/prod
-chmod -R a+rw /app/var/cache
-chown www-data:www-data -R /app/var/cache
-
-#php bin/console cache:clear
-#php bin/console assets:install
-
-printenv > /etc/environment
+mkdir -p /app/var/cache/dev
 mkdir -p /app/var/log
 chmod -R a+rw /app/var
 chown www-data:www-data -R /app/var
+
+printenv > /etc/environment
 
 exec supervisord -c /etc/supervisor/conf.d/supervisord.conf
