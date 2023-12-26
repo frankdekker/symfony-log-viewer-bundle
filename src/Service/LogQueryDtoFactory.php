a6e21e2cb7b5d1d0a23119d0d3ae67e893846071
Frank Dekker <fdekker@123inkt.nl>
2023-12-26 15:25:05 +0100

    Refactor and apply code split

--- src/Service/LogQueryDtoFactory.php
+++ src/Service/LogQueryDtoFactory.php
@@ -15,19 +15,22 @@ class LogQueryDtoFactory
      */
     public function create(Request $request, array $levels, array $channels): LogQueryDto
     {
-        $fileIdentifier   = $request->query->get('file', '');
-        $offset           = $request->query->get('offset');
-        $offset           = $offset === null || $offset === '0' ? null : (int)$offset;
-        $query            = $request->query->get('query', '');
-        $direction        = DirectionEnum::from($request->query->get('direction', 'desc'));
-        $selectedLevels   = array_filter(explode(',', $request->query->get('levels', '')), static fn($level) => $level !== '');
-        $selectedChannels = array_filter(explode(',', $request->query->get('channels', '')), static fn($channel) => $channel !== '');
-        $perPage          = $request->query->getInt('per_page', 25);
+        $fileIdentifier = $request->query->get('file', '');
+        $offset         = $request->query->get('offset');
+        $offset         = $offset === null || $offset === '0' ? null : (int)$offset;
+        $query          = $request->query->get('query', '');
+        $direction      = DirectionEnum::from($request->query->get('direction', 'desc'));
+        $perPage        = $request->query->getInt('per_page', 25);
 
-        if (count($levels) === count($selectedLevels)) {
+        // levels
+        $selectedLevels = array_filter(explode(',', $request->query->getString('levels')), static fn($level) => $level !== '');
+        if (count($levels) === count($selectedLevels) || count($selectedLevels) === 0) {
             $selectedLevels = null;
         }
-        if (count($channels) === count($selectedChannels)) {
+
+        // channels
+        $selectedChannels = array_filter(explode(',', $request->query->getString('channels')), static fn($channel) => $channel !== '');
+        if (count($channels) === count($selectedChannels) || count($selectedChannels) === 0) {
             $selectedChannels = null;
         }
 
