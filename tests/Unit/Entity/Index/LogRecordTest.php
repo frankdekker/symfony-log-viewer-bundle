2eb75cb017151a0778c3c44c085a071fd51ef18f
Frank Dekker <fdekker@123inkt.nl>
2023-12-31 11:58:31 +0100

    Add coverage

---
+++ tests/Unit/Entity/Index/LogRecordTest.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace FD\SymfonyLogViewerBundle\Tests\Unit\Entity\Index;
+
+use FD\SymfonyLogViewerBundle\Entity\Index\LogRecord;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(LogRecord::class)]
+class LogRecordTest extends TestCase
+{
+
+    public function testJsonSerialize(): void
+    {
+        $record = new LogRecord(946684800, 'debug', 'request', 'message', ['context' => 'context'], ['extra' => 'extra']);
+
+        static::assertSame(
+            [
+                'datetime'    => '2000-01-01 00:00:00',
+                'level_name'  => 'Debug',
+                'level_class' => 'text-info',
+                'channel'     => 'request',
+                'text'        => 'message',
+                'context'     => ['context' => 'context'],
+                'extra'       => ['extra' => 'extra'],
+            ],
+            $record->jsonSerialize()
+        );
+    }
+}
