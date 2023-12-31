9198badf6af5f8013f8ac7078d73d94f41ae319f
Frank Dekker <fdekker@123inkt.nl>
2023-12-31 11:53:10 +0100

    Add coverage

---
+++ tests/Unit/Entity/Index/PerformanceStatsTest.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace FD\SymfonyLogViewerBundle\Tests\Unit\Entity\Index;
+
+use FD\SymfonyLogViewerBundle\Entity\Index\PerformanceStats;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(PerformanceStats::class)]
+class PerformanceStatsTest extends TestCase
+{
+    public function testJsonSerialize(): void
+    {
+        $stats = new PerformanceStats('1.23GB', '1.23s', '1.0.1');
+
+        static::assertSame(
+            [
+                'memoryUsage' => '1.23GB',
+                'requestTime' => '1.23s',
+                'version'     => '1.0.1',
+            ],
+            $stats->jsonSerialize()
+        );
+    }
+}
