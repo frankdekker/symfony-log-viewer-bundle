014149e1806b2c2ddda09538f81bde24f753e423
Frank Dekker <fdekker@123inkt.nl>
2024-01-01 20:36:51 +0100

    Add coverage

--- tests/Unit/Service/File/Monolog/MonologFileParserTest.php
+++ tests/Unit/Service/File/Monolog/MonologFileParserTest.php
@@ -3,33 +3,34 @@ declare(strict_types=1);
 
 namespace FD\SymfonyLogViewerBundle\Tests\Unit\Service\File\Monolog;
 
+use FD\SymfonyLogViewerBundle\Entity\Config\FinderConfig;
+use FD\SymfonyLogViewerBundle\Entity\Config\LogFilesConfig;
 use FD\SymfonyLogViewerBundle\Entity\Index\LogIndex;
 use FD\SymfonyLogViewerBundle\Entity\LogFile;
+use FD\SymfonyLogViewerBundle\Entity\LogFolder;
 use FD\SymfonyLogViewerBundle\Entity\Request\LogQueryDto;
-use FD\SymfonyLogViewerBundle\Service\File\LogFileService;
 use FD\SymfonyLogViewerBundle\Service\File\LogParser;
 use FD\SymfonyLogViewerBundle\Service\File\Monolog\MonologFileParser;
+use FD\SymfonyLogViewerBundle\Service\File\Monolog\MonologLineParser;
 use Monolog\Logger;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use SplFileInfo;
 
 #[CoversClass(MonologFileParser::class)]
 class MonologFileParserTest extends TestCase
 {
     private Logger&MockObject $logger;
-    private LogFileService&MockObject $fileService;
     private LogParser&MockObject $logParser;
     private MonologFileParser $parser;
 
     protected function setUp(): void
     {
         parent::setUp();
-        $this->logger      = $this->createMock(Logger::class);
-        $this->fileService = $this->createMock(LogFileService::class);
-        $this->logParser   = $this->createMock(LogParser::class);
-        $this->parser      = new MonologFileParser([$this->logger], $this->fileService, $this->logParser);
+        $this->logger    = $this->createMock(Logger::class);
+        $this->logParser = $this->createMock(LogParser::class);
+        $this->parser    = new MonologFileParser([$this->logger], $this->logParser);
     }
 
     public function testGetLevels(): void
@@ -53,26 +54,18 @@ class MonologFileParserTest extends TestCase
         static::assertSame(['app' => 'app'], $this->parser->getChannels());
     }
 
-    public function testGetLogIndexUnknown(): void
-    {
-        $query = new LogQueryDto('identifier');
-
-        $this->fileService->expects(self::once())->method('findFileByIdentifier')->with('identifier')->willReturn(null);
-
-        $this->expectException(NotFoundHttpException::class);
-        $this->expectExceptionMessage('Log file with id `identifier` not found.');
-        $this->parser->getLogIndex($query);
-    }
-
     public function testGetLogIndex(): void
     {
-        $query = new LogQueryDto('identifier');
-        $file  = new LogFile('identifier', 'path', 'relative', 123, 111, 222);
-        $index = new LogIndex();
+        $config   = new LogFilesConfig('logName', 'monolog', 'name', $this->createMock(FinderConfig::class), true, 'patternA', 'patternB', 'Y-m-d');
+        $logQuery = new LogQueryDto('identifier');
+        $file     = new LogFile('identifier', 'path', 'relative', 123, 111, 222, $this->createMock(LogFolder::class));
+        $index    = new LogIndex();
 
-        $this->fileService->expects(self::once())->method('findFileByIdentifier')->with('identifier')->willReturn($file);
-        $this->logParser->expects(self::once())->method('parse')->willReturn($index);
+        $this->logParser->expects(self::once())
+            ->method('parse')
+            ->with(new SplFileInfo('path'), new MonologLineParser('patternA', 'patternB'), $logQuery)
+            ->willReturn($index);
 
-        static::assertSame($index, $this->parser->getLogIndex($query));
+        static::assertSame($index, $this->parser->getLogIndex($config, $file, $logQuery));
     }
 }
