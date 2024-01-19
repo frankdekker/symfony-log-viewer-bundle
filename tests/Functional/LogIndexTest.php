f5e32e297705e37a87b35e000da701aed7d2c811
Frank Dekker <fdekker@123inkt.nl>
2024-01-19 22:16:16 +0100

    Add functional test

--- tests/Functional/LogIndexTest.php
+++ tests/Functional/LogIndexTest.php
@@ -3,13 +3,26 @@ declare(strict_types=1);
 
 namespace FD\LogViewer\Tests\Functional;
 
+use FD\LogViewer\Service\JsonManifestAssetLoader;
 use PHPUnit\Framework\Attributes\CoversNothing;
+use PHPUnit\Framework\MockObject\MockObject;
 
 #[CoversNothing]
 class LogIndexTest extends AbstractFunctionalTestCase
 {
+    private JsonManifestAssetLoader&MockObject $assetLoader;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->assetLoader = $this->createMock(JsonManifestAssetLoader::class);
+        $this->getContainer()->set(JsonManifestAssetLoader::class, $this->assetLoader);
+    }
+
     public function testInvoke(): void
     {
+        $this->assetLoader->expects(self::exactly(2))->method('getUrl')->willReturn('url1', 'url2');
+
         $this->client->request('GET', '/log-viewer/');
         static::assertResponseIsSuccessful();
     }
