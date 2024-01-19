360af44c1ac429d6c7b5643c1c86e39df5f8222b
Frank Dekker <fdekker@123inkt.nl>
2024-01-19 22:08:41 +0100

    Add functional test

--- src/Service/JsonManifestAssetLoader.php
+++ src/Service/JsonManifestAssetLoader.php
@@ -21,12 +21,20 @@ class JsonManifestAssetLoader
     public function getUrl(string $asset): string
     {
         if ($this->manifestData === null) {
+            if (file_exists($this->manifestPath) === false) {
+                throw new RuntimeException('Asset manifest file does not exist: ' . $this->manifestPath . '. Run "php bin/console assets:install".');
+            }
+
+            if (is_readable($this->manifestPath) === false) {
+                throw new RuntimeException('Asset manifest file is not readable: ' . $this->manifestPath);
+            }
+
             $data = json_decode((string)file_get_contents($this->manifestPath), true, 512, JSON_THROW_ON_ERROR);
             assert(is_array($data));
             $this->manifestData = $data;
         }
 
-        $asset = $this->manifestData[$asset]['file'] ?? throw new RuntimeException('Asset manifest file does not exist: ' . $asset);
+        $asset = $this->manifestData[$asset]['file'] ?? throw new RuntimeException('Asset file does not exist: ' . $asset);
 
         return '/bundles/fdlogviewer/' . $asset;
     }
