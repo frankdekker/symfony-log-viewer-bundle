d2251c8f58a8faff81590b88cf3c559693e27681
Frank Dekker <fdekker@123inkt.nl>
2024-01-19 22:46:30 +0100

    Add functional test

--- tests/Functional/LogRecordsTest.php
+++ tests/Functional/LogRecordsTest.php
@@ -6,7 +6,7 @@ namespace FD\LogViewer\Tests\Functional;
 use PHPUnit\Framework\Attributes\CoversNothing;
 
 #[CoversNothing]
-class FoldersTest extends AbstractFunctionalTestCase
+class LogRecordsTest extends AbstractFunctionalTestCase
 {
     protected function setUp(): void
     {
@@ -15,12 +15,13 @@ class FoldersTest extends AbstractFunctionalTestCase
 
     public function testInvoke(): void
     {
-        $this->client->request('GET', '/log-viewer/api/folders');
+        $this->client->request('GET', '/log-viewer/api/logs?file=85d222d3');
         static::assertResponseIsSuccessful();
 
-        /** @var array<array{files: array<array{name: string}>}> $data */
+        /** @var array{channels: mixed[], levels: mixed[], logs: mixed[]} $data */
         $data = static::responseToArray($this->client->getResponse());
-        static::assertTrue($data[0]['files'][0]['name']);
-        static::assertSame('test.log', $data[0]['files'][0]['name']);
+        static::assertCount(2, $data['channels']);
+        static::assertCount(2, $data['levels']);
+        static::assertCount(3, $data['logs']);
     }
 }
