604c820f7fad1d0399aae7b80c2ff635fd6b5443
Frank Dekker <fdekker@123inkt.nl>
2024-01-20 15:12:11 +0100

    Fix coverage

--- tests/Unit/Service/JsonManifestAssetLoaderTest.php
+++ tests/Unit/Service/JsonManifestAssetLoaderTest.php
@@ -14,19 +14,20 @@ use RuntimeException;
 class JsonManifestAssetLoaderTest extends TestCase
 {
     private JsonManifestAssetLoader $strategy;
+    private string $path;
 
     protected function setUp(): void
     {
         parent::setUp();
         $manifest       = json_encode(['/path/to/file' => ['file' => 'file']]);
-        $path           = vfsStream::setup('root', 0777, ['manifest.json' => $manifest])->url();
-        $this->strategy = new JsonManifestAssetLoader($path . '/manifest.json');
+        $this->path     = vfsStream::setup('root', 0777, ['manifest.json' => $manifest])->url();
+        $this->strategy = new JsonManifestAssetLoader($this->path . '/manifest.json');
     }
 
     /**
      * @throws JsonException
      */
-    public function testGetVersionExistingFile(): void
+    public function testGetUrlExistingFile(): void
     {
         static::assertSame('/bundles/fdlogviewer/file', $this->strategy->getUrl('/path/to/file'));
     }
@@ -34,7 +35,31 @@ class JsonManifestAssetLoaderTest extends TestCase
     /**
      * @throws JsonException
      */
-    public function testGetVersionFailed(): void
+    public function testGetUrlMissingManifestFile(): void
+    {
+        $strategy = new JsonManifestAssetLoader('foobar');
+
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('Asset manifest file does not exist:');
+        $strategy->getUrl('foobar');
+    }
+
+    /**
+     * @throws JsonException
+     */
+    public function testGetUrlNonReadableManifest(): void
+    {
+        chmod($this->path . '/manifest.json', 0000);
+
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('Asset manifest file is not readable:');
+        $this->strategy->getUrl('foobar');
+    }
+
+    /**
+     * @throws JsonException
+     */
+    public function testGetUrlFailed(): void
     {
         $this->expectException(RuntimeException::class);
         $this->expectExceptionMessage('Asset file does not exist:');
