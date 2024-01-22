29bc7162930e11b9cf2a0347431b82de09424cba
Frank Dekker <fdekker@123inkt.nl>
2024-01-22 21:42:53 +0100

    Word parser

--- src/Service/Parser/StringParser.php
+++ src/Service/Parser/StringParser.php
@@ -5,28 +5,16 @@ namespace FD\LogViewer\Service\Parser;
 
 class StringParser
 {
-    public const WHITESPACE = [' ' => true, "\t" => true, "\n" => true, "\r" => true];
-
-    public function __construct(private QuotedStringParser $quotedStringParser)
+    public function __construct(private readonly QuotedStringParser $quotedStringParser, private readonly WordParser $wordParser)
     {
     }
 
     public function parse(StringReader $string): string
     {
-        $char = $string->peek();
-        if ($char === '"' || $char === "'") {
-            return $this->quotedStringParser->parse($string, $char, '\\');
+        if (in_array($string->get(), ['"', "'"], true)) {
+            return $this->quotedStringParser->parse($string, $string->get(), '\\');
         }
 
-        $result = '';
-        for (; $string->eol() === false; $string->next()) {
-            $char = $string->get();
-            if (isset(self::WHITESPACE[$char])) {
-                break;
-            }
-            $result .= $char;
-        }
-
-        return $result;
+        return $this->wordParser->parse($string);
     }
 }
---
+++ tests/Unit/Service/Parser/StringParserTest.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace FD\LogViewer\Tests\Unit\Service\Parser;
+
+use FD\LogViewer\Service\Parser\StringParser;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(StringParser::class)]
+class StringParserTest extends TestCase
+{
+    public function testParse(): void
+    {
+    }
+}
