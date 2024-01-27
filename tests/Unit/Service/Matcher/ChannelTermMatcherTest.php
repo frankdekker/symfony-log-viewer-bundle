a2afd71d182cc9f87b054ae8d37c5df426e5bc1d
Frank Dekker <fdekker@123inkt.nl>
2024-01-27 22:54:12 +0100

    Add coverage

---
+++ tests/Unit/Service/Matcher/ChannelTermMatcherTest.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+namespace FD\LogViewer\Tests\Unit\Service\Matcher;
+
+use DateTimeImmutable;
+use FD\LogViewer\Entity\Expression\ChannelTerm;
+use FD\LogViewer\Entity\Expression\DateBeforeTerm;
+use FD\LogViewer\Entity\Index\LogRecord;
+use FD\LogViewer\Service\Matcher\ChannelTermMatcher;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(ChannelTermMatcher::class)]
+class ChannelTermMatcherTest extends TestCase
+{
+    private ChannelTermMatcher $matcher;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->matcher = new ChannelTermMatcher();
+    }
+
+    public function testSupports(): void
+    {
+        static::assertFalse($this->matcher->supports(new DateBeforeTerm(new DateTimeImmutable())));
+        static::assertTrue($this->matcher->supports(new ChannelTerm(['app'])));
+    }
+
+    public function testMatches(): void
+    {
+        $record = new LogRecord(2000000, 'Warning', 'app', 'message', [], []);
+
+        static::assertTrue($this->matcher->matches(new ChannelTerm(['app']), $record));
+        static::assertTrue($this->matcher->matches(new ChannelTerm(['app', 'request']), $record));
+        static::assertFalse($this->matcher->matches(new ChannelTerm(['http_client', 'request']), $record));
+    }
+}
---
+++ tests/Unit/Service/Matcher/SeverityTermMatcherTest.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+namespace FD\LogViewer\Tests\Unit\Service\Matcher;
+
+use DateTimeImmutable;
+use FD\LogViewer\Entity\Expression\DateBeforeTerm;
+use FD\LogViewer\Entity\Expression\SeverityTerm;
+use FD\LogViewer\Entity\Index\LogRecord;
+use FD\LogViewer\Service\Matcher\SeverityTermMatcher;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(SeverityTermMatcher::class)]
+class SeverityTermMatcherTest extends TestCase
+{
+    private SeverityTermMatcher $matcher;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->matcher = new SeverityTermMatcher();
+    }
+
+    public function testSupports(): void
+    {
+        static::assertFalse($this->matcher->supports(new DateBeforeTerm(new DateTimeImmutable())));
+        static::assertTrue($this->matcher->supports(new SeverityTerm(['warning'])));
+    }
+
+    public function testMatches(): void
+    {
+        $record = new LogRecord(2000000, 'Warning', 'channel', 'message', [], []);
+
+        static::assertTrue($this->matcher->matches(new SeverityTerm(['warning']), $record));
+        static::assertTrue($this->matcher->matches(new SeverityTerm(['warning', 'error']), $record));
+        static::assertFalse($this->matcher->matches(new SeverityTerm(['info', 'error']), $record));
+    }
+}
