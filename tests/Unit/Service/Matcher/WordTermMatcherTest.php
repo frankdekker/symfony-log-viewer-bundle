f2e35007a7afc6bc8fb3e933d138ed539d774521
Frank Dekker <fdekker@123inkt.nl>
2024-01-27 16:35:39 +0100

    Add coverage

---
+++ tests/Unit/Service/Matcher/WordTermMatcherTest.php
@@ -0,0 +1,50 @@
+<?php
+declare(strict_types=1);
+
+namespace FD\LogViewer\Tests\Unit\Service\Matcher;
+
+use DateTimeImmutable;
+use FD\LogViewer\Entity\Expression\DateAfterTerm;
+use FD\LogViewer\Entity\Expression\WordTerm;
+use FD\LogViewer\Entity\Index\LogRecord;
+use FD\LogViewer\Service\Matcher\WordTermMatcher;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(WordTermMatcher::class)]
+class WordTermMatcherTest extends TestCase
+{
+    private WordTermMatcher $matcher;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->matcher = new WordTermMatcher();
+    }
+
+    public function testSupports(): void
+    {
+        static::assertTrue($this->matcher->supports(new WordTerm('string', WordTerm::TYPE_INCLUDE)));
+        static::assertFalse($this->matcher->supports(new DateAfterTerm(new DateTimeImmutable())));
+    }
+
+    public function testMatchesIncludes(): void
+    {
+        $wordTerm = new WordTerm('string', WordTerm::TYPE_INCLUDE);
+        $recordA  = new LogRecord(2000000, 'error', 'channel', 'message', [], []);
+        $recordB  = new LogRecord(2000000, 'error', 'channel', 'string string', [], []);
+
+        static::assertFalse($this->matcher->matches($wordTerm, $recordA));
+        static::assertTrue($this->matcher->matches($wordTerm, $recordB));
+    }
+
+    public function testMatchesExcludes(): void
+    {
+        $wordTerm = new WordTerm('string', WordTerm::TYPE_EXCLUDE);
+        $recordA  = new LogRecord(2000000, 'error', 'channel', 'message', [], []);
+        $recordB  = new LogRecord(2000000, 'error', 'channel', 'string string', [], []);
+
+        static::assertTrue($this->matcher->matches($wordTerm, $recordA));
+        static::assertFalse($this->matcher->matches($wordTerm, $recordB));
+    }
+}
