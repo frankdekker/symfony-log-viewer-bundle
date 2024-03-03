9251d136e91d01a84a57c7431b34e9509c7297cc
Frank Dekker <fdekker@123inkt.nl>
2024-03-03 09:08:54 +0100

    Ensure reload loads the correct file tree

--- frontend/src/components/FileTree.vue
+++ frontend/src/components/FileTree.vue
@@ -3,9 +3,12 @@ import LogFolder from '@/components/LogFolder.vue';
 import bus from '@/services/EventBus';
 import {useFolderStore} from '@/stores/folders';
 import {useHostsStore} from '@/stores/hosts';
+import {watch} from 'vue';
 
 const folderStore = useFolderStore();
-const hostsStore = useHostsStore();
+const hostsStore  = useHostsStore();
+
+watch(() => hostsStore.selected, () => folderStore.update());
 
 bus.on('file-deleted', () => folderStore.update());
 bus.on('folder-deleted', () => folderStore.update());
@@ -16,10 +19,7 @@ bus.on('folder-deleted', () => folderStore.update());
     <div class="p-1 pe-2 overflow-auto">
         <div class="slv-control-layout m-0">
             <div>
-                <select class="form-control p-0 border-0"
-                        v-model="hostsStore.selected"
-                        v-on:change="folderStore.update"
-                        v-if="Object.keys(hostsStore.hosts).length > 0">
+                <select class="form-control p-0 border-0" v-model="hostsStore.selected" v-if="Object.keys(hostsStore.hosts).length > 0">
                     <option v-for="(name, key) in hostsStore.hosts" :value="key" :key="key">{{ name }}</option>
                 </select>
             </div>
