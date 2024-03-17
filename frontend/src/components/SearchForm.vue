92c0c4324e705d723ab121537661cea3ad537759
Frank Dekker <fdekker@123inkt.nl>
2024-03-17 15:59:31 +0100

    Add search filter form

--- frontend/src/components/SearchForm.vue
+++ frontend/src/components/SearchForm.vue
@@ -6,6 +6,7 @@ const searchRef = ref<HTMLInputElement>();
 const query     = defineModel<string>('query');
 const sort      = defineModel<string>('sort');
 const perPage   = defineModel<string>('perPage');
+const emit      = defineEmits(['navigate']);
 
 const focus = (): void => {
     searchRef.value?.focus();
@@ -28,23 +29,15 @@ defineExpose({focus});
                placeholder="Search log entries."
                aria-label="Search log entries."
                aria-describedby="button-search"
-               @change="$emit('navigate', null)"
+               @change="emit('navigate')"
                v-model="query">
 
-        <select class="slv-menu-sort-direction form-control"
-                aria-label="Sort direction"
-                title="Sort direction"
-                v-model="sort"
-                @change="$emit('navigate', null)">
+        <select class="slv-menu-sort-direction form-control" aria-label="Sort direction" title="Sort direction" v-model="sort" @change="emit('navigate')">
             <option value="desc">Newest First</option>
             <option value="asc">Oldest First</option>
         </select>
 
-        <select class="slv-menu-page-size form-control"
-                aria-label="Entries per page"
-                title="Entries per page"
-                v-model="perPage"
-                @change="$emit('navigate', null)">
+        <select class="slv-menu-page-size form-control" aria-label="Entries per page" title="Entries per page" v-model="perPage" @change="emit('navigate')">
             <option value="50">50</option>
             <option value="100">100</option>
             <option value="150">150</option>
@@ -53,7 +46,7 @@ defineExpose({focus});
             <option value="300">300</option>
         </select>
 
-        <button class="slv-log-search-btn btn btn-outline-primary" type="button" id="button-search" @click="$emit('navigate', null)">Search</button>
+        <button class="slv-log-search-btn btn btn-outline-primary" type="button" id="button-search" @click="emit('navigate')">Search</button>
     </div>
 </template>
 
