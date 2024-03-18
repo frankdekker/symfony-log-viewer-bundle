955e9935b9248d2b63965dd0f2c318f4936dbc7d
Frank Dekker <fdekker@123inkt.nl>
2024-03-18 19:26:17 +0100

    Initialize default for hosts store selection (#71)
    * Initialize default for hosts store selection
    
    * Auto build assets [skip ci]
    
    ---------
    
    Co-authored-by: frankdekker <frankdekker@users.noreply.github.com>

--- frontend/src/stores/hosts.ts
+++ frontend/src/stores/hosts.ts
@@ -3,7 +3,7 @@ import {ref} from 'vue'
 
 export const useHostsStore = defineStore('hosts', () => {
     const hosts    = ref<{ [key: string]: string }>(JSON.parse(document.head.querySelector<HTMLMetaElement>('[name=hosts]')?.content ?? '[]'));
-    const selected = ref(Object.keys(hosts.value)[0]);
+    const selected = ref(Object.keys(hosts.value)[0] ?? 'localhost');
 
     return {hosts, selected}
 });
