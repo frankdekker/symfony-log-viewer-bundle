0d67a3f50014b2f5269c2d52260a358607c74997
Frank Dekker <fdekker@123inkt.nl>
2024-08-17 22:41:28 +0200

    Add coverage

--- tests/Unit/Iterator/LogRecordIteratorTest.php
+++ tests/Unit/Iterator/LogRecordIteratorTest.php
@@ -6,6 +6,7 @@ namespace FD\LogViewer\Tests\Unit\Iterator;
 use ArrayIterator;
 use FD\LogViewer\Entity\Index\LogRecord;
 use FD\LogViewer\Iterator\LogRecordIterator;
+use FD\LogViewer\Service\File\DateTimeParser;
 use FD\LogViewer\Service\File\LogLineParserInterface;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\MockObject\MockObject;
@@ -14,12 +15,14 @@ use PHPUnit\Framework\TestCase;
 #[CoversClass(LogRecordIterator::class)]
 class LogRecordIteratorTest extends TestCase
 {
+    private DateTimeParser&MockObject $dateTimeParser;
     private LogLineParserInterface&MockObject $lineParser;
 
     protected function setUp(): void
     {
         parent::setUp();
-        $this->lineParser = $this->createMock(LogLineParserInterface::class);
+        $this->dateTimeParser = $this->createMock(DateTimeParser::class);
+        $this->lineParser     = $this->createMock(LogLineParserInterface::class);
     }
 
     public function testGetIteratorShouldYieldErrorFromParser(): void
@@ -28,22 +31,22 @@ class LogRecordIteratorTest extends TestCase
 
         $this->lineParser->expects(self::once())->method('parse')->with('message')->willReturn(null);
 
-        $recordIterator = new LogRecordIterator($iterator, $this->lineParser);
-
+        $recordIterator = new LogRecordIterator($iterator, $this->dateTimeParser, $this->lineParser);
         $expectedRecord = new LogRecord('78e731027d8fd50ed642340b7c9a63b3', 0, 'error', 'parse', 'message', [], []);
         static::assertEquals([$expectedRecord], iterator_to_array($recordIterator));
     }
 
-    public function testGetIterator(): void
+    public function testGetIteratorWithInvalidDate(): void
     {
         $iterator = new ArrayIterator(['message']);
 
+        $this->dateTimeParser->expects(self::once())->method('parse')->with('2009-02-13 23:31:30')->willReturn(null);
         $this->lineParser->expects(self::once())
             ->method('parse')
             ->with('message')
             ->willReturn(
                 [
-                    'date'     => date('Y-m-d H:i:s', 111111),
+                    'date'     => '2009-02-13 23:31:30',
                     'severity' => 'debug',
                     'channel'  => 'request',
                     'message'  => 'message',
@@ -52,8 +55,33 @@ class LogRecordIteratorTest extends TestCase
                 ]
             );
 
-        $recordIterator = new LogRecordIterator($iterator, $this->lineParser);
-        $expectedRecord = new LogRecord('2d51458c3b1f974fae79ef1ce3d7e919', 111111, 'debug', 'request', 'message', [], []);
+        $recordIterator = new LogRecordIterator($iterator, $this->dateTimeParser, $this->lineParser);
+        $expectedRecord = new LogRecord('78e731027d8fd50ed642340b7c9a63b3', 0, 'error', 'bad-date', 'message', [], []);
+
+        static::assertEquals([$expectedRecord], iterator_to_array($recordIterator));
+    }
+
+    public function testGetIterator(): void
+    {
+        $iterator = new ArrayIterator(['message']);
+
+        $this->dateTimeParser->expects(self::once())->method('parse')->with('2009-02-13 23:31:30')->willReturn(1234567890);
+        $this->lineParser->expects(self::once())
+            ->method('parse')
+            ->with('message')
+            ->willReturn(
+                [
+                    'date'     => '2009-02-13 23:31:30',
+                    'severity' => 'debug',
+                    'channel'  => 'request',
+                    'message'  => 'message',
+                    'context'  => [],
+                    'extra'    => [],
+                ]
+            );
+
+        $recordIterator = new LogRecordIterator($iterator, $this->dateTimeParser, $this->lineParser);
+        $expectedRecord = new LogRecord('ef088a39731e4d450d8692fdcc811415', 1234567890, 'debug', 'request', 'message', [], []);
 
         static::assertEquals([$expectedRecord], iterator_to_array($recordIterator));
     }
