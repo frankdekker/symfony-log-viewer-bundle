935aa3a6de9d1e6e976a89e9e2ce1caa4c3780b5
Frank Dekker <fdekker@123inkt.nl>
2024-08-30 22:53:24 +0200

    Show date picker on start and end date

--- frontend/src/components/datepicker/DatePicker.vue
+++ frontend/src/components/datepicker/DatePicker.vue
@@ -4,10 +4,12 @@ import DatePickerDropdown from '@/components/datepicker/DatePickerDropdown.vue';
 import type DateSelection from '@/models/DateSelection';
 import {reactive, ref, watch} from 'vue';
 
-const expanded = ref(false);
+const active       = ref(false);
+const dateExpanded = ref<'none' | 'startDate' | 'endDate'>('none');
 defineProps<{ value: string }>();
 const emit    = defineEmits(['input']);
-const startDate = reactive<DateSelection>({date: new Date(), formatted: ''});
+let startDate = reactive<DateSelection>({date: new Date(), formatted: ''});
+let endDate   = reactive<DateSelection>({date: new Date(), formatted: 'now'});
 
 watch(startDate, () => {
     console.log('selection', startDate.date, startDate.formatted);
@@ -16,11 +18,28 @@ watch(startDate, () => {
 
 <template>
     <div>
-        <button class="slv-date-picker-btn form-control dropdown-toggle" type="button" :aria-expanded="expanded" @click="expanded = !expanded">
+        <button class="slv-date-picker-btn form-control dropdown-toggle"
+                type="button"
+                @click="dateExpanded = 'startDate';active = true"
+                v-if="!active">
             <i class="bi bi-calendar3"></i>
-            {{ startDate.formatted }}
         </button>
-        <date-picker-dropdown v-if="expanded" :class="{'d-block': expanded}" active-tab="relative" v-model="startDate" label="Start date"/>
+        <button class="slv-date-picker-btn form-control dropdown-toggle" type="button" v-if="active">
+            <i class="bi bi-calendar3"></i>
+            <span class="ms-1" @click="dateExpanded = 'startDate' ? 'none' : 'startDate'">{{ startDate.formatted }}</span>
+            ~
+            <span @click="dateExpanded = dateExpanded === 'endDate' ? 'none' : 'endDate'">{{ endDate.formatted }}</span>
+        </button>
+        <date-picker-dropdown v-if="dateExpanded === 'startDate'"
+                              :class="{'d-block': dateExpanded === 'startDate'}"
+                              active-tab="relative"
+                              v-model="startDate"
+                              label="Start date"/>
+        <date-picker-dropdown v-if="dateExpanded === 'endDate'"
+                              :class="{'d-block': dateExpanded === 'endDate'}"
+                              active-tab="now"
+                              v-model="endDate"
+                              label="End date"/>
     </div>
 </template>
 
