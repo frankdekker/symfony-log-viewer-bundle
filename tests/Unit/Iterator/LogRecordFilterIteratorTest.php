23b7e47be371ee9edc9be6624fac14dcc54248d5
Frank Dekker <fdekker@123inkt.nl>
2024-12-25 22:24:40 +0100

    Add support for date range filter in the backend

--- tests/Unit/Iterator/LogRecordFilterIteratorTest.php
+++ tests/Unit/Iterator/LogRecordFilterIteratorTest.php
@@ -6,6 +6,7 @@ namespace FD\LogViewer\Tests\Unit\Iterator;
 use ArrayIterator;
 use FD\LogViewer\Entity\Expression\Expression;
 use FD\LogViewer\Entity\Index\LogRecord;
+use FD\LogViewer\Entity\Request\SearchQuery;
 use FD\LogViewer\Iterator\LogRecordFilterIterator;
 use FD\LogViewer\Service\Matcher\LogRecordMatcher;
 use PHPUnit\Framework\Attributes\CoversClass;
@@ -29,11 +30,11 @@ class LogRecordFilterIteratorTest extends TestCase
         $infoRecord     = new LogRecord('id', 222222, 'info', 'app', 'message', [], []);
         $warningRecord  = new LogRecord('id', 333333, 'warning', 'event', 'message', [], []);
         $recordIterator = new ArrayIterator([$debugRecord, $infoRecord, $warningRecord]);
-        $expression     = new Expression([]);
+        $searchQuery    = new SearchQuery(new Expression([]));
 
         $this->recordMatcher->expects(self::exactly(3))->method('matches')->with()->willReturn(true, false, false);
 
-        $iterator = new LogRecordFilterIterator($this->recordMatcher, $recordIterator, $expression);
+        $iterator = new LogRecordFilterIterator($this->recordMatcher, $recordIterator, $searchQuery);
         static::assertSame([$debugRecord], array_values(iterator_to_array($iterator)));
     }
 }
