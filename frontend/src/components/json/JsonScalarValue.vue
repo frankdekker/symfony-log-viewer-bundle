dd229e8e38232a6b93ceea407e7fd6ea5ba74bbe
Frank Dekker <fdekker@123inkt.nl>
2025-06-16 23:49:47 +0200

    Add json styling

--- frontend/src/components/json/JsonScalarValue.vue
+++ frontend/src/components/json/JsonScalarValue.vue
@@ -5,8 +5,8 @@ const props = defineProps<{ value: unknown }>();
 </script>
 
 <template>
-    <div v-if="props.value === null" class="text-info">null</div>
-    <div v-else-if="typeof props.value === 'boolean'" class="text-info">{{ props.value ? 'true' : 'false' }}</div>
-    <div v-else-if="Numbers.numeric(value)" class="text-info">{{ props.value }}</div>
-    <div v-else>{{ props.value }}</div>
+    <div v-if="props.value === null" class="text-info" title="type: null">null</div>
+    <div v-else-if="typeof props.value === 'boolean'" class="text-info" title="type: boolean">{{ props.value ? 'true' : 'false' }}</div>
+    <div v-else-if="Numbers.numeric(value)" class="text-info" :title="'type: ' + typeof props.value">{{ props.value }}</div>
+    <div v-else :title="'type: ' + typeof props.value">{{ props.value }}</div>
 </template>
